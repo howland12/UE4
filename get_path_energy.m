ef27abd8f647f63d2d2f3b1235bb652774dc7247
Dominik Wieland <wielan_d@itp.tugraz.at>
2018-05-16 16:01:41 +0200

    optimized get_path_energy

--- get_path_energy.m
+++ get_path_energy.m
@@ -1,22 +1,17 @@
 function [ E ] = get_path_energy( town_distribution )
 %GET_PATH_ENERGY Summary of this function goes here
 %   Detailed explanation goes here
-    x_coordinates = town_distribution(:,1);
-    y_coordinates = town_distribution(:,2);
-
-    x_p1 = zeros([1 length(x_coordinates)])';
-    x_p1(1:end-1) = x_coordinates(2:end);
-    x_p1(end) = x_coordinates(1);
+    x_p1 = zeros( [1 length( town_distribution(:,1) ) ])';
+    x_p1(1:end-1) = town_distribution(2:end,1);
+    x_p1(end) = town_distribution(1,1);
     
-    y_p1 = zeros([1 length(x_coordinates)])';
-    y_p1(1:end-1) = y_coordinates(2:end);
-    y_p1(end) = y_coordinates(1);
+    y_p1 = zeros([1 length( town_distribution(:,2) ) ])';
+    y_p1(1:end-1) = town_distribution(2:end,2);
+    y_p1(end) = town_distribution(1,2);
     
-    x_diff_vector = (x_p1 - x_coordinates).^2;
-    y_diff_vector = (y_p1 - y_coordinates).^2;
+    x_p1 = (x_p1 - town_distribution(:,1)).^2; % ( x_diff^2 = x(i+1) - x(i) )^2
+    y_p1 = (y_p1 - town_distribution(:,2)).^2; % ( y_diff^2 = y(i+1) - y(i) )^2
     
-    modulus_vector = sqrt(x_diff_vector + y_diff_vector);
-    
-    E = sum(modulus_vector);
+    E =sum(sqrt(x_p1 + y_p1));
 end
 
